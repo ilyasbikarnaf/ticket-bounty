c3a80ccaa3b52b0c08d28af8f5859023af43e5aa
ilyasbikarnaf <ilyas.bikarnaf@gmail.com>
2026-01-29 08:31:24 +0100

    chore: setup lucia auth, radix ui, and update prisma schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,16 @@
+import { hash } from "@node-rs/argon2";
 import prisma from "@/lib/prisma";
+
+const users = [
+  {
+    username: "admin",
+    email: "admin@admin.com",
+  },
+  {
+    username: "ilyas",
+    email: "9441hamou@gmail.com",
+  },
+];
 
 const tickets = [
   {
@@ -28,10 +40,17 @@ const seed = async () => {
   const t0 = performance.now();
   console.log("Starting seed....");
 
+  await prisma.user.deleteMany({});
   await prisma.ticket.deleteMany({});
 
+  const passwordHash = await hash("secret");
+
+  const dbUsers = await prisma.user.createManyAndReturn({
+    data: users.map((user) => ({ ...user, passwordHash })),
+  });
+
   for (const ticket of tickets) {
-    await prisma.ticket.create({ data: ticket });
+    await prisma.ticket.create({ data: { ...ticket, userId: dbUsers[0].id } });
   }
 
   const t1 = performance.now();
---
+++ src/lib/auth.ts
@@ -0,0 +1,84 @@
+import { hash, verify } from "@node-rs/argon2";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { signinPath } from "@/app/path";
+import { getAuth } from "@/features/auth/actions/get-auth";
+import { lucia } from "@/lib/lucia";
+import prisma from "./prisma";
+
+type RegisterData = {
+  username: string;
+  email: string;
+  password: string;
+};
+
+export const registerUser = async ({
+  email,
+  username,
+  password,
+}: RegisterData) => {
+  const passwordHash = await hash(password);
+
+  const user = await prisma.user.create({
+    data: {
+      email,
+      passwordHash,
+      username,
+    },
+  });
+
+  return user;
+};
+
+type AuthenticateUserData = {
+  email: string;
+  password: string;
+};
+
+export const authenticateUser = async ({
+  email,
+  password,
+}: AuthenticateUserData) => {
+  const user = await prisma.user.findUnique({ where: { email } });
+
+  if (!user) {
+    return null;
+  }
+
+  const validPassword = await verify(user.passwordHash, password);
+
+  if (!validPassword) {
+    return null;
+  }
+  return user;
+};
+
+export const logout = async () => {
+  const { session } = await getAuth();
+
+  if (!session) {
+    return false;
+  }
+
+  await lucia.invalidateSession(session.id);
+
+  const sessionCookie = lucia.createBlankSessionCookie();
+  (await cookies()).set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes,
+  );
+
+  return true;
+};
+
+export const setSession = async (userId: string) => {
+  const session = await lucia.createSession(userId, {});
+  const sessionCookie = lucia.createSessionCookie(session.id);
+
+  (await cookies()).set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes,
+  );
+};
